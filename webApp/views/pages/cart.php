33b9e51f686c9c01887302deb005042d9715cb48
frank-the-tankk <frafzhong111@gmail.com>
2023-11-06 21:47:38 -0600

    Shopping Cart

---
+++ webApp/views/pages/cart.php
@@ -0,0 +1,46 @@
+
+
+<h1>Shopping Cart</h1>
+
+<table class="cart">
+    <thead>
+        <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>Erlenmeyer Flask, 250mL</td>
+            <td>$7.36</td>
+            <td>1</td>
+            <td>$7.36</td>
+
+        </tr>
+        <tr>
+            <td>Erlenmeyer Flask, 350mL</td>
+            <td>$7.36</td>
+            <td>1</td>
+            <td>$7.36</td>
+           
+        </tr>
+    </tbody>
+</table>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Buttons</title>
+</head>
+
+<body>
+    <div class="button-container">
+        <button class="button">Return to Shop</button>
+        <button class="button">Update Cart</button>
+    </div>
+</body>
