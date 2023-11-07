347e82906fbb709d18da70163e0f8fabd18ef11e
Leonardo Céspedes <lecespedes@estudiantec.cr>
2023-11-06 21:38:35 -0600

    Products, product pages

---
+++ webApp/views/pages/product.php
@@ -0,0 +1,39 @@
+<div class="product-details">
+    <div class="detail">
+        <div class="main-info">
+            <div class="product-images">
+                <div class="main-image">
+                    <img loading="lazy" src="/images/erlenmeyer.png" alt="product">
+                </div>
+                <div class="small-image">
+                    <img loading="lazy" src="/images/erlenmeyer.png" alt="product">
+                </div>
+            </div>
+        </div>
+        <div class="product-info">
+            <p class="info-name">Erlenmeyer Flask, 250mL</p>
+            <p class="info-price">US $7.36<span>+Shipping: US $20.3</span></p>
+            <p class="info-description">Elevate your laboratory experiments with our 250mL Erlenmeyer Flask. Crafted from durable borosilicate glass, it's designed for precise mixing, heating, and storage in the lab. Upgrade your equipment today for better results!</p>
+        </div>
+    </div>
+    <div class="shipping">
+        <form>
+            <div class="shipping__address">
+                <p>Ship to</p>
+                <i class='bx bx-map'></i>
+                <p>Los Angeles</p>
+            </div>
+            <div class="shipping__details">
+                <p>Delivery <i class='bx bx-right-arrow-alt' ></i></p>
+                <p>+Shipping: <span>US $20.3</span></p>
+                <p class="delivery">14 - day delivery on orders over US $20</p>
+                <p class="date">delivery by <span>Nov 26 - Nov 28</span></p>
+            </div>
+            <div class="quantity">
+                <label for="quantity">Quantity:</label>
+                <input type="number" id="quantity" name="quantity" value="1" min="1">
+            </div>
+            <input class="add" type="submit" value="Add to Cart">
+        </form>
+    </div>
+</div>
--- webApp/views/pages/products.php
+++ webApp/views/pages/products.php
@@ -1,3 +1,41 @@
 <div class="products">
     <h1>Our Products</h1>
+    <div class="products__list">
+        <a class="product" href="/product">
+            <div class="product__image">
+                <img loading="lazy" src="/images/erlenmeyer.png" alt="product">
+            </div>
+            <p class="product__name">Erlenmeyer Flask, 250mL</p>
+            <p class="product__price">US $7.36</p>
+        </a> <!-- product -->
+        <a class="product" href="/product">
+            <div class="product__image">
+                <img loading="lazy" src="/images/erlenmeyer.png" alt="product">
+            </div>
+            <p class="product__name">Erlenmeyer Flask, 250mL</p>
+            <p class="product__price">US $7.36</p>
+        </a> <!-- product -->
+        <a class="product" href="/product">
+            <div class="product__image">
+                <img loading="lazy" src="/images/erlenmeyer.png" alt="product">
+            </div>
+            <p class="product__name">Erlenmeyer Flask, 250mL</p>
+            <p class="product__price">US $7.36</p>
+        </a> <!-- product -->
+        <a class="product" href="/product">
+            <div class="product__image">
+                <img loading="lazy" src="/images/erlenmeyer.png" alt="product">
+            </div>
+            <p class="product__name">Erlenmeyer Flask, 250mL</p>
+            <p class="product__price">US $7.36</p>
+        </a> <!-- product -->
+        <a class="product" href="/product">
+            <div class="product__image">
+                <img loading="lazy" src="/images/erlenmeyer.png" alt="product">
+            </div>
+            <p class="product__name">Erlenmeyer Flask, 250mL</p>
+            <p class="product__price">US $7.36</p>
+        </a> <!-- product -->
+    </div> <!-- products__list -->
+   
 </div>
