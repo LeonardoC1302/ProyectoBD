4f722b5ae88f2ddaab39d70f0f51bc81e8a8306d
Erika Michelle Cerdas Mejias <ecerdasmejias04@gmail.com>
2023-11-08 20:31:04 -0600

    forgot password page

---
+++ webApp/views/auth/forgot.php
@@ -0,0 +1,20 @@
+<div class="login-container">
+    <div class="image-login">
+        <img loading="lazy" src="/images/forgot.png" alt="forgot image">
+    </div>
+    <div class="login-form">
+        <p class="form-title">Forgot Password?</p>
+        <p class="form-description">Enter your email and we'll send you a link to reset your password.</p>
+        <form class="form">
+            <div class="form__field">
+                <input class="form__input-login" type="email" id="email" placeholder="Enter your email" name="email">
+            </div>
+
+            <input class="form__submit-100" type="submit" value="Submit">
+            <div class="actions">
+                <a href="/login" class="action">Back to <span>Login</span></a>
+            </div>  
+
+        </form>
+    </div>
+</div>
