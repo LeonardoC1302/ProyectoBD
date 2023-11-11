cb1ff0381da066c8650f3cd6d20476e7bdaa3731
frank-the-tankk <frafzhong111@gmail.com>
2023-11-11 14:45:29 -0600

    Checkout page styles

--- webApp/views/pages/checkout.php
+++ webApp/views/pages/checkout.php
@@ -1,1 +1,33 @@
-<h1>Checkout</h1>
+<div class="checkout">
+    <div class="checkout__productsPrice">
+        <p class="total"> Total</p>
+        <p class="price">$134.00</p> 
+        
+        <div class="checkout__productsInfo">
+            <div class="checkout__productsInfo__productRow">
+                <img loading="lazy" src="/images/erlenmeyer.png" >
+                <div class="text">
+                    <p class="prodName"> Erlenmeyer Flask, 250mL</p>
+                    <p class="prodQuantity"> Quantity: 1</p>
+                </div>
+                <p class="prodPrice"> $60</p>
+                
+
+            </div>
+            <div class="checkout__productsInfo__productRow">
+                <img loading="lazy" src="/images/erlenmeyer.png" >
+                <div class="text">
+                    <p class="prodName"> Erlenmeyer Flask, 250mL</p>
+                    <p class="prodQuantity"> Quantity: 1</p>
+                </div>
+                <p class="prodPrice"> $60</p>
+                
+
+            </div>
+            <p class="subTotal"> Sub total: </p>
+        </div> 
+        
+    </div>
+
+</div>
+
