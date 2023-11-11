e44064a2e9b1adcaf268e7c5e02b6889b8daac43
Erika Cerdas <ercerdas@estudiantec.cr>
2023-11-11 16:41:16 -0600

    customer service client view

---
+++ webApp/views/help/service.php
@@ -0,0 +1,16 @@
+<div class="service">
+    <h1 class="service_title">Customer Service</h1>
+    <div class="service__info">
+        <img loading="lazy" src="/images/support.png" alt="service image">
+        <div class="service__description">
+            <p class="description__title">Do you have problems with your order or just a complaint?</p>
+            <p>Ask us anything and we'll help you find answers</p>
+            <div class="title-info">
+                <i class='bx bx-phone icon-contact'></i>
+                <p>Call To Us</p>
+            </div>
+            <p>We are available 24/7, 7 days a week</p>
+            <p>Phone: +506 88888888</p>
+        </div>
+    </div>
+</div>
