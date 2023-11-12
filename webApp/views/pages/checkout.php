4ab6cc27e8c0830a4a0beca70f685b8da04c0b9e
frank-the-tankk <frafzhong111@gmail.com>
2023-11-11 22:18:12 -0600

    Checkout form

--- webApp/views/pages/checkout.php
+++ webApp/views/pages/checkout.php
@@ -1,33 +1,70 @@
 <div class="checkout">
-    <div class="checkout__productsPrice">
-        <p class="total"> Total</p>
-        <p class="price">$134.00</p> 
-        
-        <div class="checkout__productsInfo">
-            <div class="checkout__productsInfo__productRow">
-                <img loading="lazy" src="/images/erlenmeyer.png" >
-                <div class="text">
-                    <p class="prodName"> Erlenmeyer Flask, 250mL</p>
-                    <p class="prodQuantity"> Quantity: 1</p>
-                </div>
-                <p class="prodPrice"> $60</p>
-                
 
-            </div>
-            <div class="checkout__productsInfo__productRow">
-                <img loading="lazy" src="/images/erlenmeyer.png" >
-                <div class="text">
-                    <p class="prodName"> Erlenmeyer Flask, 250mL</p>
-                    <p class="prodQuantity"> Quantity: 1</p>
-                </div>
-                <p class="prodPrice"> $60</p>
-                
+    <div class="leftBlock">
+        <div class="leftBlock__productsPrice">
+            <p class="total"> Total</p>
+            <p class="price">$134.00</p> 
+            
+            <div class="leftBlock__productsInfo">
+                <div class="leftBlock__productsInfo__productRow">
+                    <img loading="lazy" src="/images/erlenmeyer.png" >
+                    <div class="text">
+                        <p class="prodName"> Erlenmeyer Flask, 250mL</p>
+                        <p class="prodQuantity"> Quantity: 1</p>
+                    </div>
+                    <p class="prodPrice"> $60</p>
+                    
 
-            </div>
-            <p class="subTotal"> Sub total: </p>
-        </div> 
-        
+                </div>
+                <div class="leftBlock__productsInfo__productRow">
+                    <img loading="lazy" src="/images/erlenmeyer.png" >
+                    <div class="text">
+                        <p class="prodName"> Erlenmeyer Flask, 250mL</p>
+                        <p class="prodQuantity"> Quantity: 1</p>
+                    </div>
+                    <p class="prodPrice"> $60</p>
+                    
+
+                </div>
+
+                <div class="leftBlock__productsInfo__productRow">                
+                    <p class="subTotal"> Sub total: </p>
+                    <p class="subTotalPrice"> $60</p>
+                </div>
+
+                <div class="leftBlock__productsInfo__productRow">                
+                    <p class="shipping"> Shipping: </p>
+                    <p class="shippingPrice"> $60</p>
+                </div>
+
+                <div class="leftBlock__productsInfo__productRow">                
+                    <p class="total"> Total Due:  </p>
+                    <p class="totalPrice"> $60</p>
+                </div>
+
+                
+            </div> 
+            
+        </div>
+
     </div>
 
-</div>
 
+    <div class="rightBlock">
+        <h4 >Shipping Information</h4>
+        <div class="form__field">
+            <label for="email" class="form__label">Email:</label>
+            <input class="form__input" type="email" id="email" placeholder="Your email" name="email">
+        </div>
+        <div class="form__field">
+            <label for="address" class="form__label">Shipping Address:</label>
+            <input class="form__input" type="text" id="address" placeholder="Your address" name="address">
+        </div>
+       
+    </div>
+
+
+
+
+
+</div>
