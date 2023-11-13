eb110ff78f17275a36207d8f8fc762965b2df65f
Kevin Chang <kchang@estudiantec.cr>
2023-11-12 19:54:02 -0600

    fixes

--- webApp/views/admin/employeeReport.php
+++ webApp/views/admin/employeeReport.php
@@ -1,1 +1,10 @@
-<h1>Employee Report</h>
+<h1 class="line">Employee Report</h>
+
+<div class="actions">
+    <a href="/admin/employeeSearch" class="link-btn">Return Search Page</a>
+    <a class="line"> Today's Date: <?php echo date('y-m-d') ?> </a>
+    <a href="/admin/employees" class="link-btn"> Return to Employee List </a>
+</div>
+
+<div>
+</div>
