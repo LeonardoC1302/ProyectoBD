e4c82c89e4fb8cb59d9f9ea7700ed27acb3eb44f
frank-the-tankk <frafzhong111@gmail.com>
2023-11-16 21:33:23 -0600

    Sql Server changes v2

--- SQLServer/ScriptCreacion.sql
+++ SQLServer/ScriptCreacion.sql
@@ -1,6 +1,122 @@
 USE
+    [master] GO
+    /****** Object:  Database [storage]    Script Date: 16/11/2023 21:26:45 ******/
+CREATE DATABASE
+    [storage] CONTAINMENT = NONE ON PRIMARY (
+        NAME = N 'storage',
+        FILENAME = N 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLSERVER\MSSQL\DATA\storage.mdf',
+        SIZE = 8192 KB,
+        MAXSIZE = UNLIMITED,
+        FILEGROWTH = 65536 KB
+    ) LOG ON (
+        NAME = N 'storage_log',
+        FILENAME = N 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLSERVER\MSSQL\DATA\storage_log.ldf',
+        SIZE = 8192 KB,
+        MAXSIZE = 2048 GB,
+        FILEGROWTH = 65536 KB
+    )
+WITH
+    CATALOG_COLLATION = DATABASE_DEFAULT,
+    LEDGER = OFF GO
+ALTER DATABASE [storage]
+SET
+    COMPATIBILITY_LEVEL = 160 GO IF (
+        1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled')
+    ) begin EXEC [storage].[dbo].[sp_fulltext_database] @action = 'enable'
+end GO
+ALTER DATABASE [storage]
+SET ANSI_NULL_DEFAULT OFF GO
+ALTER DATABASE [storage]
+SET ANSI_NULLS OFF GO
+ALTER DATABASE [storage]
+SET ANSI_PADDING OFF GO
+ALTER DATABASE [storage]
+SET ANSI_WARNINGS OFF GO
+ALTER DATABASE [storage]
+SET ARITHABORT OFF GO
+ALTER DATABASE [storage]
+SET AUTO_CLOSE OFF GO
+ALTER DATABASE [storage]
+SET AUTO_SHRINK OFF GO
+ALTER DATABASE [storage]
+SET
+    AUTO_UPDATE_STATISTICS ON GO
+ALTER DATABASE [storage]
+SET
+    CURSOR_CLOSE_ON_COMMIT OFF GO
+ALTER DATABASE [storage]
+SET CURSOR_DEFAULT GLOBAL GO
+ALTER DATABASE [storage]
+SET
+    CONCAT_NULL_YIELDS_NULL OFF GO
+ALTER DATABASE [storage]
+SET NUMERIC_ROUNDABORT OFF GO
+ALTER DATABASE [storage]
+SET QUOTED_IDENTIFIER OFF GO
+ALTER DATABASE [storage]
+SET RECURSIVE_TRIGGERS OFF GO
+ALTER DATABASE [storage]
+SET DISABLE_BROKER GO
+ALTER DATABASE [storage]
+SET
+    AUTO_UPDATE_STATISTICS_ASYNC OFF GO
+ALTER DATABASE [storage]
+SET
+    DATE_CORRELATION_OPTIMIZATION OFF GO
+ALTER DATABASE [storage]
+SET TRUSTWORTHY OFF GO
+ALTER DATABASE [storage]
+SET
+    ALLOW_SNAPSHOT_ISOLATION OFF GO
+ALTER DATABASE [storage]
+SET PARAMETERIZATION SIMPLE GO
+ALTER DATABASE [storage]
+SET
+    READ_COMMITTED_SNAPSHOT OFF GO
+ALTER DATABASE [storage]
+SET
+    HONOR_BROKER_PRIORITY OFF GO
+ALTER DATABASE [storage]
+SET RECOVERY FULL GO
+ALTER DATABASE [storage]
+SET MULTI_USER GO
+ALTER DATABASE [storage]
+SET PAGE_VERIFY CHECKSUM GO
+ALTER DATABASE [storage]
+SET DB_CHAINING OFF GO
+ALTER DATABASE [storage]
+SET
+    FILESTREAM(NON_TRANSACTED_ACCESS = OFF) GO
+ALTER DATABASE [storage]
+SET
+    TARGET_RECOVERY_TIME = 60 SECONDS GO
+ALTER DATABASE [storage]
+SET
+    DELAYED_DURABILITY = DISABLED GO
+ALTER DATABASE [storage]
+SET
+    ACCELERATED_DATABASE_RECOVERY = OFF GO EXEC sys.sp_db_vardecimal_storage_format N 'storage',
+    N 'ON' GO
+ALTER DATABASE [storage]
+SET QUERY_STORE = ON GO
+ALTER DATABASE [storage]
+SET
+    QUERY_STORE (
+        OPERATION_MODE = READ_WRITE,
+        CLEANUP_POLICY = (
+            STALE_QUERY_THRESHOLD_DAYS = 30
+        ),
+        DATA_FLUSH_INTERVAL_SECONDS = 900,
+        INTERVAL_LENGTH_MINUTES = 60,
+        MAX_STORAGE_SIZE_MB = 1000,
+        QUERY_CAPTURE_MODE = AUTO,
+        SIZE_BASED_CLEANUP_MODE = AUTO,
+        MAX_PLANS_PER_QUERY = 200,
+        WAIT_STATS_CAPTURE_MODE = ON
+    ) GO
+USE
     [storage] GO
-    /****** Object:  Table [dbo].[addresses]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[addresses]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -19,7 +135,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[affinities]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[affinities]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -38,7 +154,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[carts]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[carts]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -55,7 +171,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[countries]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[countries]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -73,7 +189,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[currencies]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[currencies]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -91,7 +207,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[employees]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[employees]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -110,7 +226,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[payments]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[payments]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -132,7 +248,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[products]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[products]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -156,7 +272,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[productsXCart]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[productsXCart]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -176,7 +292,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[productTypes]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[productTypes]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -193,7 +309,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[productXsales]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[productXsales]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -213,7 +329,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[roles]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[roles]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -231,7 +347,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[sales]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[sales]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -251,7 +367,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[users]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[users]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -276,7 +392,7 @@ CREATE TABLE
                 OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
             ) ON [PRIMARY]
     ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY] GO
-    /****** Object:  Table [dbo].[warehouses]    Script Date: 16/11/2023 21:22:23 ******/
+    /****** Object:  Table [dbo].[warehouses]    Script Date: 16/11/2023 21:26:46 ******/
 SET ANSI_NULLS ON GO
 SET QUOTED_IDENTIFIER ON GO
 CREATE TABLE
@@ -397,3 +513,6 @@ ADD
     CONSTRAINT [FK_warehouses_addresses] FOREIGN KEY([addressId]) REFERENCES [dbo].[addresses] ([id]) GO
 ALTER TABLE
     [dbo].[warehouses] CHECK CONSTRAINT [FK_warehouses_addresses] GO
+USE [master] GO
+ALTER DATABASE [storage]
+SET READ_WRITE GO
